fbf89b13baff3edaf94558083014882e63de284d
BexCdz <beckypearce92@hotmail.com>
2026-03-17 21:09:20 +0100

    Added footer to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <footer className="footer">
+        Coded by Becky Pearce, open-sourced on <a href="https://github.com/BexCdz/my-app">GitHub</a>
+      </footer>
     </div>
+    
   );
 }
